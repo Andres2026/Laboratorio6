1fb021b88503d9546d8e531d15692b1fcc115a83
Andres2026 <andres.aguirreflrs@uanl.edu.mx>
2023-10-21 17:32:23 -0600

    Se finalizo la paigna de tienda, se añadio el JS al index y a noticias

--- js/index.js
+++ js/index.js
@@ -2,3 +2,9 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
+function toggle(obj) {
+    var obj=document.getElementById(btnPop);
+    if (obj.style.display == "block") obj.style.display = "none";
+    else obj.style.display = "block";
+}
